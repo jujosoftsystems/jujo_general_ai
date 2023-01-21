886df9dcd76162586d9c85806b15eba615851a71
RobCM <robert.coleman.martinez@gmail.com>
2023-01-21 10:11:40 -0600

    Some quotes added to random quotes abilitie.

--- api/random_quotes.php
+++ api/random_quotes.php
@@ -6,8 +6,27 @@
         $error_msg = "None all good"; // <-- default
         $result = ""; // <-- default
         
-        $result = rand(1, 5);
+        $rand_quote_num = rand(1, 6);
 
+        switch ($rand_quote_num){
+            case 1:
+                $result = "A lady's imagination is very rapid; it jumps from admiration to love, from love to matrimony in a moment.  -Jane Austen";
+              break;
+            case 2:
+                $result = "AI might be superior to humans.";
+              break;
+            case 3:
+                $result = "Our lives begin to end the day we become silent about things that matter.  -Martin Luther King Jr.";
+              break;
+            case 4:
+                $result = "Faith is taking the first step even when you can't see the whole staircase.  -Martin Luther King Jr.";
+              break;
+            case 5:
+                $result = "If you can't explain it to a six year old, you don't understand it yourself.  -Albert Einstein";
+              break;
+            default:
+                $result = "Sorry Can't think of any quotes right now!";
+        }
 
         // Array for API!
         $random_quotes_abilitie_response = array(
