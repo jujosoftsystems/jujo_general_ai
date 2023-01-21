66a5ca39bd34229dae9e5dbb3ce0ad199d1bbe13
RobCM <robert.coleman.martinez@gmail.com>
2023-01-20 18:31:39 -0600

    .

--- api/random_quotes.php
+++ api/random_quotes.php
@@ -3,7 +3,22 @@
         Random quotes API 
     */
     function random_quotes_abilitie(){
+        $error_msg = "None all good"; // <-- default
+        $result = ""; // <-- default
+        
+        $result = rand(1, 5);
 
+
+        // Array for API!
+        $random_quotes_abilitie_response = array(
+            "api_id"=> "2",
+            "result"=> $result,
+            "error"=> $error_msg
+        );
+
+        // JSON array data
+        header("Content-Type: application/json");
+        print(json_encode($random_quotes_abilitie_response, JSON_PRETTY_PRINT));
     }
 
     random_quotes_abilitie();
