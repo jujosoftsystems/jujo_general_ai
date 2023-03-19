4004bbe338ee20556c4332b0b97c9a7599014887
RobCM <robert.coleman.martinez@gmail.com>
2023-03-19 09:24:26 -0500

    .

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
        example to call API's:
        for count --->  http://192.168.1.182/api/count.php?startnum=41&endnum=99
        for random quotes ---> http://192.168.1.182/api/random_quotes.php
-       for weather ---> http://192.168.1.182/api/weather.php?place=Austin
+       for weather ---> http://192.168.1.182/api/weather.php?place=Bastrop TX
     */
 
     echo "-- Welcome to Jujosoft's General AI Project --<br>";
