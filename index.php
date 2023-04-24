55d09a2fb773be72a0bffcf7e08584ee027861d6
RobCM <robert.coleman.martinez@gmail.com>
2023-04-24 18:55:19 -0500

    .

--- index.php
+++ index.php
@@ -11,10 +11,10 @@
 
     echo "-- Welcome to Jujosoft's General AI Project --<br>";
     echo "current abilities <br>";
-    echo " * count <br>";
-    echo " * random quotes <br>";
-    echo " * weather <br>";
-    echo " * news <br>";
-    echo " * find me <br>";
+    echo " * <a href=\"http://192.168.1.182/api/count.php?startnum=41&endnum=99\">count</a> <br>";
+    echo " * <a href=\"http://192.168.1.182/api/random_quotes.php\">random quotes</a> <br>";
+    echo " * <a href=\"http://192.168.1.182/api/weather.php?place=Bastrop TX\">weather</a> <br>";
+    echo " * <a href=\"http://192.168.1.182/api/news.php?topic=science\">news</a> <br>";
+    echo " * <a href=\"http://192.168.1.182/api/findme.php\">find me</a> <br>";
     
 ?>
