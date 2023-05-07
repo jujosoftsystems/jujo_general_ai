8f6da1489ab9c4b7cdbe6507692ac42a6428c889
RobCM <robert.coleman.martinez@gmail.com>
2023-05-07 16:16:16 -0500

    .

--- api/crypto.php
+++ api/crypto.php
@@ -4,23 +4,59 @@
 
         to get crypto prices
 
-        https://api.coincap.io/v2/assets
+        https://www.coinlore.com/cryptocurrency-data-api            <--- Docs about this external API
+
+        https://api.coinlore.net/api/tickers/?start=0&limit=100     <--- To get top 100 crypto coins (rank 1 to 100!)
+        https://api.coinlore.net/api/tickers/?start=100&limit=100   <--- To get coins from rank 100 to rank 200 crypto coins
+        https://api.coinlore.net/api/ticker/?id=90                  <--- BTC id example
+
     */
 
-     // Call external news API
-     $response_1 = file_get_contents('https://api.coincap.io/v2/assets');
-     $api_1_result = json_decode($response_1, true);
+    function crypto_abilitie(){
 
-     $result = "";
+        $error_msg = "None all good"; // <-- default
+        $result = ""; // <-- default
 
-    // Array for API!
-    $crypto_abilitie_response = array(
-        "api_id"=> "4",
-        "result"=>  $api_1_result,
-        "error"=> $error_msg
-    );
+        // Call external news API
+        $response_1 = file_get_contents('https://api.coinlore.net/api/tickers/?start=0&limit=100');
+        $api_1_result = json_decode($response_1, true);
 
-    // JSON array data
-    header("Content-Type: application/json");
-    print(json_encode($crypto_abilitie_response, JSON_PRETTY_PRINT));
+        $response_2 = file_get_contents('https://api.coinlore.net/api/tickers/?start=100&limit=100');
+        $api_2_result = json_decode($response_2, true);
+
+        // Combine 2 external APIs to one array for the data we need!
+        $result_array_1 = [];
+        $result_array_2 = [];
+        for($i = 0; $i<= 100; $i++){
+            foreach($api_1_result as $index_1){
+                array_push($result_array_1, "Coin:".$index_1[$i]["name"]."Price:".$index_1[$i]["price_usd"]);
+            }
+        }
+        
+        $result_array = [];
+        for($i = 0; $i<= 100; $i++){
+            foreach($api_2_result as $index_2){
+                array_push($result_array_2, "Coin:".$index_2[$i]["name"]."Price:".$index_2[$i]["price_usd"]);
+            }
+        }
+
+        // Clean array
+        $result_combine = array_merge($result_array_1 , $result_array_2);
+        // Final array for result
+        $result = array_unique( $result_combine); // <--- still need to clean  "1": "Coin:Price:", from final array!
+
+        // Array for API!
+        $crypto_abilitie_response = array(
+            "api_id"=> "6",
+            "result"=>  $result,
+            "error"=> $error_msg
+        );
+
+        // JSON array data
+        header("Content-Type: application/json");
+        print(json_encode($crypto_abilitie_response, JSON_PRETTY_PRINT));
+
+    }
+
+    crypto_abilitie(); 
 ?>
